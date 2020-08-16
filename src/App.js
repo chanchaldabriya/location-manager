92fbcd1665c641ae9c880d16828d973ca11babe4
chanchaldabriya <chanchaldabriya@gmail.com>
2020-08-16 15:04:39 +0530

    Added routes that will be used in the app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route path="/location/:id" render={() => <h1>Hello</h1>} />
+
+        <Route exact path="/" component={Home} />
+
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
